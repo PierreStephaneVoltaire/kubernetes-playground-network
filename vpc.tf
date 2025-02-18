40d39348e27ccd7ab539c2bf905042f20522d128
Pierre-Stephane Voltaire <psvoltaire96@gmail.com>
2025-02-18 00:57:40 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -14,3 +14,59 @@ module "vpc" {
     "kubernetes.io/subnets/public":var.app_name
   }
 }
+
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+locals {
+  regions=toset([data.aws_region.current.name,"us-east-1"])
+}
+
+module "vpc_endpoints" {
+  source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  vpc_id = module.vpc.vpc_id
+  create_security_group      = true
+  security_group_name_prefix = "${var.app_name}-vpc-endpoints-"
+  security_group_description = "VPC endpoint security group"
+  security_group_rules = {
+    ingress_https = {
+      description = "HTTPS from VPC"
+      cidr_blocks = flatten([module.vpc.vpc_cidr_block,module.vpc.vpc_secondary_cidr_blocks])
+    }
+  }
+
+  endpoints = {
+    s3 = {
+      service             = "s3"
+      private_dns_enabled = true
+      service_type    = "Gateway"
+    },
+    ecr_api = {
+      service             = "ecr.api"
+      private_dns_enabled = true
+      subnet_ids          = slice(module.vpc.private_subnets,3,6)
+    },
+    ecr_dkr = {
+      service             = "ecr.dkr"
+      private_dns_enabled = true
+      subnet_ids          =slice(module.vpc.private_subnets,3,6)
+    },
+
+    logs = {
+      service             = "logs"
+      private_dns_enabled = true
+      subnet_ids          = slice(module.vpc.private_subnets,0,3)
+    },
+    cloudtrail = {
+      service             = "cloudtrail"
+      private_dns_enabled = true
+      subnet_ids          = slice(module.vpc.private_subnets,0,3)
+    },
+    ebs = {
+      service             = "ebs"
+      private_dns_enabled = true
+      subnet_ids          = slice(module.vpc.private_subnets,0,3)
+    },
+  }
+
+}
